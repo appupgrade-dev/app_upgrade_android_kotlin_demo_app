a6871ad5a733d1fb9e6b8b1a6b09174e3aeb49ec
App Upgrade <appupgrade.dev@gmail.com>
2023-03-01 18:25:52 +0530

    Bump up SDK

--- app/src/main/java/com/appupgrade/app_upgrade_android_kotlin_demo_app/MainActivity.kt
+++ app/src/main/java/com/appupgrade/app_upgrade_android_kotlin_demo_app/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.appupgrade.app_upgrade_android_sdk.AppUpgrade
 import com.appupgrade.app_upgrade_android_sdk.models.AppInfo
+import com.appupgrade.app_upgrade_android_sdk.models.PreferredAndroidMarket
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +17,14 @@ class MainActivity : AppCompatActivity() {
         val xApiKey = "ZWY0ZDhjYjgtYThmMC00NTg5LWI0NmUtMjM5OWZkNjkzMzQ5"
 
         val appInfo = AppInfo(
-            appId = "com.android.com",
+            appId = "com.snapdeal.main",
             appName = "Wallpaper app",
             appVersion = "1.0.0",
             platform = "android",
             environment = "production",
-            appLanguage = "es" //Optional. Your app language code.
+            appLanguage = "es", //Optional. Your app language code.
+            // preferredAndroidMarket = PreferredAndroidMarket.HUAWEI, // or PreferredAndroidMarket.AMAZON or PreferredAndroidMarket.OTHER If not provided default is Google playstore. Optional
+            // otherAndroidMarketUrl = "https://otherandroidmarketurl.com/app/id" // Required if preferredAndroidMarket is OTHER.
         )
 
         appUpgrade = AppUpgrade()
